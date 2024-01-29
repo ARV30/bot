6f30308795821f28c0a73bb0d63bbfb8b7b0deda
ARV30 <154830098+ARV30@users.noreply.github.com>
2024-01-29 21:00:39 +0400

    Add files via upload

---
+++ test_kot.py
@@ -0,0 +1,32 @@
+import telebot
+from telebot import types
+
+# Установка токена вашего бота
+bot = telebot.TeleBot("6949638859:AAGiMkN2NhPCfVZBad1208DhN6piCEBkzBM")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    # Создаем объект клавиатуры
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+
+    # Создаем кнопки выбора
+    button1 = types.KeyboardButton(text="Привет")
+    button2 = types.KeyboardButton(text="Пока")
+
+    # Добавляем кнопки на клавиатуру
+    keyboard.add(button1, button2)
+
+    # Отправляем приветственное сообщение с клавиатурой
+    bot.send_message(message.chat.id, "Привет! Я кот.", reply_markup=keyboard)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    # Обрабатываем нажатие кнопок выбора
+    if message.text == "Привет":
+        bot.reply_to(message, "Ну привет, лучше нажми кнопку пока")
+    elif message.text == "Пока":
+        bot.reply_to(message, "Молодец")
+
+# Запуск приложения
+bot.polling()
+
